2f3aadb84427787c84602a0d7d92b0e6ff287a58
AashwinSuresh <aashwinsuresh326@gmail.com>
2026-06-09 14:49:16 +0530

    Add problem description for Minimum Element After Digit Sum Replacements

---
+++ EASY/test2.py
@@ -0,0 +1,6 @@
+# Problem: Minimum Element After Digit Sum Replacements
+
+# Description:
+# You are given an integer array 'nums'.
+# You replace each element in 'nums' with the sum of its digits.
+# Return the minimum element in 'nums' after all replacements have been made.
